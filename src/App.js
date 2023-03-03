05be770cb62f171eb3d1d94310be7f27614d586b
ohamed Eldeeb <agnabiieldeeeb@gmail.com>
2023-03-03 19:32:38 +0200

    Try to add signin #2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './styles/App.css';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
     <div className="App">
+      <SignUp />
       <h1>Final Capstone</h1>
     </div>
   );
