2f1adc919bd563e8c6e4ac853baab5d1bbed28bb
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2023-03-05 18:05:49 +0000

    Configure routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './styles/App.css';
 import SignUp from './pages/SignUp';
+import LogIn from './pages/LogIn';
 
 function App() {
   return (
-    <div className="App">
-      <SignUp />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route exact path="/" element={<SignUp />} />
+          <Route exact path="/logInPage" element={<LogIn />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
