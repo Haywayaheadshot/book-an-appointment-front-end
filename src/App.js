c513b5c9840e786204202641c0f7f51345774d69
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2023-03-06 23:14:16 +0000

    Add landing page route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './styles/App.css';
 import SignUp from './pages/SignUp';
 import LogIn from './pages/LogIn';
+import LandingPage from './pages/LandingPage';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<SignUp />} />
           <Route exact path="/logInPage" element={<LogIn />} />
+          <Route exact path="/landingPage" element={<LandingPage />} />
         </Routes>
       </div>
     </Router>
