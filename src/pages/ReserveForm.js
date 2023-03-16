e815fe342e81083c19e6a2201fa9ad0deabc2cfe
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2023-03-16 15:10:39 +0000

    Fix eslint errors

--- src/pages/ReserveForm.js
+++ src/pages/ReserveForm.js
@@ -96,7 +96,6 @@ const ReserveForm = () => {
         errorEl.remove();
       }, 1000);
     } else {
-      console.log(data)
       const url = 'http://localhost:3000/api/reservations';
       const options = {
         method: 'POST',
@@ -252,7 +251,7 @@ const ReserveForm = () => {
               <option value={doc.id} key={doc.id}>{doc.name}</option>
             ))}
           </select>
-          
+
           <div id="EM" />
           <button
             className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-32 sm:w-32 px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
